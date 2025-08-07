31333bfefc0c4a87474ef475fe5a5477c1179c68
KhushpreetKaur04 <khushepawar438@gmail.com>
2025-08-07 13:00:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ model = joblib.load('model.pkl')
 scaler = joblib.load('scaler.pkl')
 
 # ✅ Set background image (Update the URL below with your own image link)
-background_image_url = "https://img.freepik.com/premium-vector/banking-service-background-bank-building-icon-made-with-currency-symbols-dollar-euro-yen-pound_127544-3053.jpg?semt=ais_hybrid"  # <-- Replace with your image URL
+background_image_url = "https://tse2.mm.bing.net/th/id/OIP.s6zddCQQEJh14qoYnu_nkQHaHa?r=0&cb=thfvnext&rs=1&pid=ImgDetMain&o=7&rm=3"  # <-- Replace with your image URL
 page_bg_img = f'''
 <style>
 .stApp {{
@@ -153,6 +153,7 @@ if 'prediction' in locals():
         file_name="loan_prediction_report.pdf",
         mime="application/pdf"
     )
+
 
 
 
